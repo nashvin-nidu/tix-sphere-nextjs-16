e7853abf604c1ae618a2f9f79cb15d3c1ed6688f
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-09 22:53:20 +0530

    modified page for display

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section> 
+      <h1 className="text-center">The Hub for Every Dev <br /> Event You Can&apos;t Miss</h1>
+      <p className="text-center mt-5">Hacthons, Meetups and Confrencess, all in one place</p>
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events list-none">
+          {events.map((event) => 
+            (<li key={event.id}>
+              <EventCard {...event} />
+            </li>))
+          }
+        </ul>
+      </div>
+    </section>
+
   );
 }
