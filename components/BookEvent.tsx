86a91731a5fb778d049be5ca98982dfa247f6218
Nashvin Nidu <nashvinnidu1111@gmail.com>
2025-11-14 02:17:22 +0530

    implemented DB uploading and posthog capture for booking

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,17 +1,24 @@
 "use client"
+import { BookingEvent } from "@/lib/actions/booking.action";
+import posthog from "posthog-js";
+
 import { useState } from "react"
 
 
-const BookEvent = () => {
+const BookEvent = ({event_id, slug} : {event_id: string, slug: string}) => {
     const [email, setEmail] = useState('');
     const [submitted, setSubmitted] = useState(false);
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        setTimeout( () => {
+        
+        const {success} = await BookingEvent({event_id, email})
+        if(success){
             setSubmitted(true);
-        }, 1000 );
+            posthog.capture("event_booking", {event_id, slug, email});
+        }else {
+            return ({ message:"Event Booking Failed"});
+        }
     }
     return(
         <div id="book-event">
